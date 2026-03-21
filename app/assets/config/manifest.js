68064be28f94e4f5b72156298e0085c77445564a
ssingh214 <ssingh214@rrc.ca>
2026-03-21 01:15:02 -0500

    Add products front page and routing - Feature 2.1

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link application.css
+//= link active_admin.css
+//= link application.js
+//= link_tree ../../../app/javascript .js
