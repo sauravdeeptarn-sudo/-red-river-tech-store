8fb4385186fbc4fd3f9e306ca3da98b92ff303d7
ssingh214 <ssingh214@rrc.ca>
2026-03-31 22:35:32 -0500

    Fix categories admin with ransack and product count - Feature 1.5

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,10 +1,15 @@
 ActiveAdmin.register Category do
   permit_params :name
 
+  config.filters = false
+
   index do
     selectable_column
     id_column
     column :name
+    column "Products Count" do |category|
+      category.products.count
+    end
     actions
   end
 
@@ -13,5 +18,12 @@ ActiveAdmin.register Category do
       f.input :name
     end
     f.actions
+  end
+
+  show do
+    attributes_table do
+      row :name
+      row :created_at
+    end
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -4,10 +4,10 @@ class Category < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   def self.ransackable_attributes(auth_object = nil)
-    ["name", "created_at", "updated_at"]
+    ["name", "created_at", "updated_at", "id"]
   end
 
   def self.ransackable_associations(auth_object = nil)
-    []
+    ["products"]
   end
 end
