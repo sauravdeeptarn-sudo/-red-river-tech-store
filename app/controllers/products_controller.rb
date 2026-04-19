271e5419124215ba4945234b8207006c0c271dbb
ssingh214 <sauravdeeptarn@gmail.com>
2026-04-19 15:34:28 -0500

    Show products with images first on home page - Feature 4.1.2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,28 +1,31 @@
 class ProductsController < ApplicationController
   def index
     @categories = Category.all
-    @products = Product.all.includes(:category, image_attachment: :blob)
+
+    with_images = Product.joins(:image_attachment).includes(:category, image_attachment: :blob)
+    without_images = Product.where.not(id: with_images.pluck(:id)).includes(:category)
+    all_products = with_images + without_images
 
     if params[:search].present?
-      @products = @products.where("name ILIKE ? OR description ILIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+      all_products = all_products.select { |p| p.name.downcase.include?(params[:search].downcase) || p.description.downcase.include?(params[:search].downcase) }
     end
 
     if params[:category_id].present? && params[:category_id] != "0"
-      @products = @products.where(category_id: params[:category_id])
+      all_products = all_products.select { |p| p.category_id == params[:category_id].to_i }
     end
 
     if params[:filter].present?
       case params[:filter]
       when "on_sale"
-        @products = @products.on_sale
+        all_products = all_products.select { |p| p.is_on_sale }
       when "new"
-        @products = @products.new_arrivals
+        all_products = all_products.select { |p| p.created_at >= 3.days.ago }
       when "recently_updated"
-        @products = @products.recently_updated
+        all_products = all_products.select { |p| p.updated_at >= 3.days.ago && p.created_at < 3.days.ago }
       end
     end
 
-    @products = @products.page(params[:page]).per(6)
+    @products = Kaminari.paginate_array(all_products).page(params[:page]).per(6)
   end
 
   def show
