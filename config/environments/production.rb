5c0732dc4442a17a9bcc94a46eb397956684b980
ssingh214 <sauravdeeptarn@gmail.com>
2026-04-19 12:38:18 -0500

    Fix secret_key_base for production asset precompilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-config.secret_key_base = ENV["SECRET_KEY_BASE"]
+config.secret_key_base = ENV["SECRET_KEY_BASE"] || ENV["SECRET_KEY_BASE_DUMMY"]
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
